f459c948e0df0b3e3f37646f8d5ec622a5f36205
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2019-11-27 18:06:09 +0100

    Classification almost done

--- Classification/classification.m
+++ Classification/classification.m
@@ -3,28 +3,37 @@ load Data_PTC_vs_FTC.mat
 gen(1,:) = Data.X(1,:);
 gen(2,:) = Data.X(24,:);
 t = Data.D;
+t(t == -1) = 0;
+
+class_0 = gen(:, logical(t == 0));
+class_1 = gen(:, logical(t == 1));
 
 figure;
-plot(gen(1,:), gen(2,:), '*');
+plot(class_0(1,:), class_0(2,:), '*');
+hold on
+plot(class_1(1,:), class_1(2,:), '*');
 
-net = newp(gen, t, 'hardlims', 'learnp');
-net.trainParam.epochs = 200;
-net = train(net, gen);
-Y = sim(net, gen); 
-%%
-numberOfSubsets = 11;
+numberOfSubsets = 86;
 numberOfDataInSubset = floor(length(gen)/(numberOfSubsets - 1));
-genSubsets = mat2cell(gen, 2, [numberOfDataInSubset * ones(1, numberOfSubsets - 1), length(gen) - (numberOfSubsets - 1) * numberOfDataInSubset]);
+numberOfDataInLastSubset = length(gen) - (numberOfSubsets - 1) * numberOfDataInSubset;
+genSubsets = mat2cell(gen, 2, [numberOfDataInSubset * ones(1, numberOfSubsets - 1), numberOfDataInLastSubset]);
 
-%%
 for subsetIndex = 1 : numberOfSubsets
     genTest = genSubsets{subsetIndex};
     if subsetIndex == 1
         genLearn = cell2mat(genSubsets(2:end));
+        classes = t(numberOfDataInSubset + 1:end);
     elseif subsetIndex == numberOfSubsets
         genLearn = cell2mat(genSubsets(1:end-1));
+        classes = t(1:end - numberOfDataInLastSubset);
     else
         genLearn = cell2mat(genSubsets([1:(subsetIndex - 1), (subsetIndex + 1):end]));
+        classes = t([1:numberOfDataInSubset*(subsetIndex - 1), (numberOfDataInSubset*subsetIndex+1):end]);
     end
-   % genLearn = cell2mat(genSubsets);
+    net = newp(genLearn, classes);
+    net.trainParam.epochs = 50;
+    net.trainParam.showWindow=false;
+    net = train(net, genLearn, classes);
+    Y{subsetIndex} = sim(net, genTest);
 end
+xD = sum(cell2mat(Y)==t);
